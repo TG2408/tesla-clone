a36ac3b4a5594b3ed95b08985ef539717fdba4f6
TG2408 <tanmaygupta2408@gmail.com>
2021-10-28 01:54:17 +0530

    model-s-y-3-x added

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,21 +1,25 @@
 import React from "react";
 import styled from "styled-components";
+  
 
-function Section() {
+function Section({ title, description, leftBtnTxt, rightBtnTxt, backgroundImg }) {
     return (
-        <Wrap>
+        <Wrap bgImage = {backgroundImg}>
           <ItemText>
-            <h1>Model S</h1>
-            <p>Order Online for Touchless Delivery</p>
+            <h1>{title}</h1>
+            <p>{description}</p>
           </ItemText>
-          <ButtonGroup>
-            <LeftButton>
-              Custom Order     
-            </LeftButton>
-            <RightButton>
-              Existing Inventory
-            </RightButton>
-          </ButtonGroup>
+            <Buttons>
+              <ButtonGroup>
+                <LeftButton>
+                  {leftBtnTxt}     
+                </LeftButton>
+                <RightButton>
+                  {rightBtnTxt}
+                </RightButton>
+              </ButtonGroup>
+              <DownArrow src="/images/down-arrow.svg" />
+            </Buttons>
         </Wrap>
     )
 }
@@ -28,7 +32,7 @@ const Wrap = styled.div`
     background-size: cover;
     background-position: center;
     background-repeat: no-repeat;
-    background-image: url('/images/images/model-s.jpg');
+    background-image: ${props => `url("/images/${props.bgImage}")`};
     display: flex;
     flex-direction: column;
     justify-content: space-between;  //vertical
@@ -43,6 +47,9 @@ const ItemText = styled.div`
 const ButtonGroup = styled.div`
   display: flex;
   margin-bottom: 30px;
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
 `
 const LeftButton = styled.div`
   background-color: rgba(23, 26, 32, 0.8);
@@ -56,8 +63,22 @@ const LeftButton = styled.div`
   text-transform: uppercase;
   font-size: 12px;
   cursor: pointer;
+  margin: 8px;
 `
 
 const RightButton = styled(LeftButton)`
+  background: white;
+  opacity: 0.65;
+  color: black;
+`
+
+const DownArrow = styled.img`
+
+  height: 40px;
+  animation: animateDown infinite 1.5s;
+  overflow-x: hidden;
+`
+
+const Buttons = styled.div`
 
 `
